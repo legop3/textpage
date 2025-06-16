0a8f1613c2ada766b72b95f9eec215a7ddfbb305
Lina Plus Plus <TenmaTengo@protonmail.com>
2025-06-16 01:10:36 -0400

    creating a pipeline of tasks for client to log in

--- src/socket.mjs
+++ src/socket.mjs
@@ -28,6 +28,7 @@ export async function start(state){
 async function onSocketConnect(socket) {
     const sessionid = sessionIDCounter++;
     let clientSlug = `nobody@socket${sessionid}`;
+    let userid = null;
 
     console.log(`${clientSlug}: connected`);
 
@@ -35,18 +36,53 @@ async function onSocketConnect(socket) {
         console.log(`${clientSlug}: disconnected`)
     })
 
-    // --- sequenced below this line
+    // --- sequenced login process below this line
 
-    let cookie = await new Promise((ok,err)=> {
-        socket.emit('getCookie',ok);
+
+    while (userid === null){
+        let cookie = await new Promise((ok,err)=> {
+            socket.emit('getCookie',ok);
+        });
+
+        cookie = String(cookie);
+
+        console.log(`${clientSlug}: got cookie: ${cookie}`);
+
+        let {userid:_userid} = await Database.getSingle('SELECT userid from cookies where cookie = ? limit 1',cookie)
+
+        if (_userid) {
+            clientSlug =
+            console.log(`${clientSlug}: cookie was valid`);
+            userid = _userid;
+            continue;
+        }
+
+        //TODO create/connect account to cookie
+        console.log("cookie is not coonnected to account, mindlessly trying again");
+
+    }
+
+    let {displayName,handle,bio} = await Database.getSingle('SELECT displayName,handle,bio from Users where id = ? limit 1',id);
+
+    await new Promise((ok,err)=> {
+        socket.emit('logged_in',ok,{
+            //id:userid,
+            displayName,handle,bio
+        });
     });
 
-    console.log(`${clientSlug}: got cookie: ${cookie}`);
+
 
     // --- register commands below this line
 
+    // commands go here...
 
-    socket.broadcast.emit('request-fulldoc')
+    socket.on('deltaUpdateSend', (delta) => {
+        console.log(`${clientSlug}: delta recieved: ${JSON.stringify(delta)}`)
+        socket.broadcast.emit('deltaUpdate', delta)
+    });
 
+    // --- register commands above this line
+    socket.broadcast.emit('accepting_commands');
 
 }
